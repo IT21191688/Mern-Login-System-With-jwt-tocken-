b8c20b4ba0641e0bf28f9151011cc58e41cc653f
IT21191688 <IT21191688@my.sliit.lk>
2023-06-20 21:53:46 +0530

    Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home';
-import SignUp from './components/SignUp';
-import Login from './components/Login';
-import Recovery from './components/Recovery';
-import Reset from './components/Reset';
-import PageNotFound from './components/PageNotFound';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-
-const router = createBrowserRouter([
-
-  {
-    path: '/',
-    element: <Home />
-  },
-  {
-
-    path: '/signup',
-    element: <SignUp />
-
-  },
-  {
-    path: '/login',
-    element: <Login />
-  },
-  {
-
-    path: '/recovery',
-    element: <Recovery />
-
-  },
-  {
-    path: '/reset',
-    element: <Reset />
-  }
-  ,
-  {
-    path: '*',
-    element: <PageNotFound />
-  }
-
-])
 
 
 function App() {
   return (
     <>
 
-      <RouterProvider router={router}></RouterProvider>
+
 
     </>
   );
