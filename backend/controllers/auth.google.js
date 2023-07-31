37a9f199b6de33428b8d104cdcc03ff35313af36
IT21191688 <IT21191688@my.sliit.lk>
2023-07-31 08:12:55 +0530

    m

--- backend/controllers/auth.google.js
+++ backend/controllers/auth.google.js
@@ -12,52 +12,28 @@ const routsInit = async (app, passport) => {
     app.get('/auth/google',
         passport.authenticate('google', { scope: ['profile', 'email'] }));
 
-
-
-
     app.get('/auth/google/callback', passport.authenticate('google', {
         failureRedirect: '/login'
     }),
         function (req, res) {
 
-            const user = req.user
-            // Generate a JWT token
+            const user = req.user;
+
             const token = jwt.sign({ userId: user._id }, config.secretKey);
             const role = user.role
 
             console.log(token, role)
 
-            //res.json({ token, role });
-
-            // Send the token and role back to the frontend
             res.send(`<script>
                      window.opener.postMessage({ token: '${token}', role: '${role}' }, 'http://localhost:3000');
                      window.close();
                     </script>`);
-            //res.cookie('token', token);
-            //res.cookie('role', role);
 
-            //res.redirect(`http://localhost:3000/userHome?token=${token}&role=${role}`);
-
-
-
-            /*
-            
-                        if (role === "user") {
-                            res.redirect("http://localhost:3000/userHome?token=" + token + "&role=" + role);
-                        } else if (role === "admin") {
-                            res.redirect("http://localhost:3000/adminHome?token=" + token + "&role=" + role);
-                        } else {
-                            res.redirect("http://localhost:3000/login");
-                        }
-            
-                        */
             console.log("User Authanticated")
         });
 
     app.get('/api/is-authenticated', googleAuthenticate, (req, res) => {
-        // If the user is authenticated, we can assume the user is logged in.
-        // Now, we can create a JWT and send it to the frontend.
+
         const user = req.user;
         const token = jwt.sign({ userId: user._id }, config.secretKey);
         const role = user.role;
